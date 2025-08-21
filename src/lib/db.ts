5225270703c9cc4462362a573726741c32545864
Amaad Ali <amaadali0304@gmail.com>
2025-08-21 09:59:04 +0000

    When click on button Error Connection failed: Failed to connect to 139.5

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,8 @@ import sql from 'mssql';
 const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  server: `${process.env.DB_HOST},${process.env.DB_PORT}`,
+  server: process.env.DB_HOST as string,
+  port: Number(process.env.DB_PORT),
   database: process.env.DB_DATABASE,
   options: {
     encrypt: process.env.DB_ENCRYPT === 'true',
