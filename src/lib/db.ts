a66b51af1dec8cb2d70d4ccdd45276526baf4ef3
Amaad Ali <amaadali0304@gmail.com>
2025-08-21 09:33:18 +0000

    this way i connect using tool

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,9 +3,8 @@ import sql from 'mssql';
 const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  server: process.env.DB_HOST || 'localhost',
+  server: `${process.env.DB_HOST},${process.env.DB_PORT}`,
   database: process.env.DB_DATABASE,
-  port: Number(process.env.DB_PORT),
   options: {
     encrypt: process.env.DB_ENCRYPT === 'true',
     trustServerCertificate: true 
