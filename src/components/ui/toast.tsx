c9395949c554e8a24621b4b113461cbc57bae7e6
Amaad Ali <amaadali0304@gmail.com>
2025-08-21 10:20:33 +0000

    #5cb85c is the color code of toast

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -33,7 +33,7 @@ const toastVariants = cva(
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
         success:
-          "success group border-accent-foreground bg-accent-foreground text-accent",
+          "success group border-transparent bg-[#5cb85c] text-primary-foreground",
       },
     },
     defaultVariants: {
@@ -79,7 +79,7 @@ const ToastClose = React.forwardRef<
   <ToastPrimitives.Close
     ref={ref}
     className={cn(
-      "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600 group-[.success]:text-accent/70 group-[.success]:hover:text-accent",
+      "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600 group-[.success]:text-primary-foreground/70 group-[.success]:hover:text-primary-foreground",
       className
     )}
     toast-close=""
