5648ccd2fc2b0d36c09a15d22fe98d1722fd923d
Amaad Ali <amaadali0304@gmail.com>
2025-08-23 09:39:49 +0000

    also adjust my logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,16 +3,14 @@ import Image from "next/image";
 export function Logo() {
   return (
     <div  className="flex items-center gap-2">
-      {/* <HeartPulse className="h-6 w-6 text-primary" /> */}
       <Image
-  src="/images/logo.png"
-  alt="My WebP Image"
-  width={80}
-  height={80}
-  priority
-
-/>
-      <h1 style={{ fontSize: "12px", fontWeight: "bold" }} className="text-sm font-bold">One Stop Healthcare Solution</h1>
+        src="/images/logo.png"
+        alt="One Stop Healthcare Solution Logo"
+        width={40}
+        height={40}
+        priority
+      />
+      <h1 className="text-sm font-bold">One Stop<br/>Healthcare Solution</h1>
     </div>
   );
 }
