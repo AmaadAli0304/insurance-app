2f979a7b95d512a9084ed530fa2135260ea4dee8
Amaad Ali <amaadali0304@gmail.com>
2025-09-07 18:06:07 +0000

    Also Remove One Stop Text from Login Page just need logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,17 +1,14 @@
-import { HeartPulse } from 'lucide-react';
 import Image from "next/image";
+
 export function Logo() {
   return (
-    <div  className="flex items-center gap-2">
-      <Image
-        src="/images/logo.png"
-        alt="One Stop Logo"
-        width={70}
-        height={70}
-        priority
-        data-ai-hint="logo"
-      />
-      <h1 className="text-lg font-bold">One Stop</h1>
-    </div>
+    <Image
+      src="/images/logo.png"
+      alt="One Stop Logo"
+      width={70}
+      height={70}
+      priority
+      data-ai-hint="logo"
+    />
   );
 }
