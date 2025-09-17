f776ede9ab8cf62e941aacfb05d7f8fe000e532d
Amaad Ali <amaadali0304@gmail.com>
2025-09-17 08:32:14 +0000

    ReferenceError: React is not defined     at [project]/src/app/dashboard/

--- src/app/dashboard/pre-auths/[id]/edit/page.tsx
+++ src/app/dashboard/pre-auths/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 
+import * as React from "react";
 import { useActionState, useEffect, useState, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
